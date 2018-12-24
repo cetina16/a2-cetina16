83d00ade28a381a358e4d3916602bd787713aef5
cetina16 <cetina16@itu.edu.tr>
2018-12-24 22:09:44 +0300

    password and template?

--- bottle_app.py
+++ bottle_app.py
@@ -1,33 +1,92 @@
-
 #####################################################################
 ### Assignment skeleton
 ### You can alter the below code to make your own dynamic website.
 ### The landing page for assignment 3 should be at /
 #####################################################################
 
-from bottle import route, run, default_app, debug
+from bottle import route, run, default_app, debug, template, request, static_file
 
-def htmlify(title,text):
+
+def home():
     page = """
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8" />
-                <title>%s</title>
-            </head>
-            <body>
-            %s
-            </body>
-        </html>
 
-    """ % (title,text)
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+		  <title> Vanlife </title>
+		<meta charset="utf-8"/>
+        <link rel= "stylesheet" href= "style.css">
+        <link rel="icon" href="vl.ico">
+        <style> 
+		.box {width:800px;
+              margin:auto;
+              width:800px;
+              background-color: lemonchiffon;
+              border: 1px solid darkblue;}
+        </style>
+		</head>
+	    <body>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ <header>
+  <img src="vl.png" alt="vanlife designed by Gimp" width="350" height="150">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ </header>
+
+ <div class="menu">
+		     <ul>
+				  <li><a href="index.html">Mainpage</a></li>
+				 <li><a href="whatisvanlife.html">WhatisVanLife</a></li>
+				 <li><a href="doityourself.html">Do it Yourself</a></li>
+				 <li><a href="types.html">Types </a></li> 
+			 </ul> </div><br>
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; 
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; 
+&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+  <div class="box">
+<p style= "font-family: Times New Roman, sans-serif ;text-align:justify;text-indent:10px;
+   text-align: center;margin:auto ; padding:20px;
+  color: darkblue; font-size: 20pt">
+Hello everyone!Every human being needs to relax 
+and feel free. Some people stay at home for relaxing.
+Get rid of stress and tiredness is easy for them.Actually, I was one of them since 
+watching videos about Vanlife.<strong>If you want to find out whether it is suitable for you,read an informative
+article about</strong> <a href="https://tinyurl.com/yc4r4x66"> <abbr title= "Is van life for you?"><i>vanlife</i></abbr></a>
+<br> There are many options. You can buy a old transporter and convert 
+  it to a dream van or you can directly buy an amazing expensive van.
+ It depends on your choises and financial conditions.<br> Live simply, live happier!</div>
+
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; 
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; 
+&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+
+<img src="van2ink.png" alt="vanimage" width="800" height="500"  >
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+<img src="vanedited.png" alt="vanimage2" width="800" height="500"  >
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; 
+	&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; 
+	&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+	<div class="footer">
+	<footer>
+		<p> contact: cetina16@itu.edu.tr</p> <br>
+ <p>references:</p>
+<a href="https://southernchronicles.icebreaker.com" target="_blank">icebraker.com </a> <br>
+<a href="https://www.influx.co.uk/" target="_blank"> influx.co </a> <br>
+<a href="https://kombilife.com/" target="_blank"> kombilife.com </a> <br>
+<a href="https://pinterest.com" target="_blank"> pinterest.com </a> <br>
+<a href="https://www.thevanual.com/" target="_blank"> thevanual.com </a>
+</footer></div>
+</body>
+</html>
+    """
     return page
 
-def index():
-    return htmlify("My lovely website",
-                   "This is going to be an awesome website, when it is finished.")
 
-route('/', 'GET', index)
+route('/', 'GET', home)
 
 #####################################################################
 ### Don't alter the below code.
@@ -41,5 +100,4 @@ debug(True)
 app = default_app()
 # The below code is necessary for running this bottle app standalone on your computer.
 if __name__ == "__main__":
-  run()
-
+    run()
---
+++ repl_comments.py
@@ -0,0 +1,27 @@
+from hashlib import sha256
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+
+my_list=[]
+num=1
+i=0
+
+password_hash = "c2e2de13717990810a222f960c2b73f7d981a7a230a8c5b05ea622ddcc6e1710"
+pw1 = input("Enter your password:")
+hsh1 = create_hash(pw1)
+
+
+if hsh1==password_hash:
+    print("login completed")
+
+    while num==1:
+        comments=input("Enter some comments:")
+        my_list.append(comments)
+        print("Previously entered comments:")
+
+        for i in range (len(my_list)):
+            print(int(i+1), ".", my_list[i])
+            i=i+1
+else:
+    print("failed login")
